e60fc02fd8b1dcf88c1650f969b391f6ad4c56bd
vlimap <valtemir.procopio@gmail.com>
2026-03-02 21:07:10 -0300

    fix: controllers

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware');
-const { googleLogin, getMe } = require('../controllers/authController');
+const AuthController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/google', authMiddleware, googleLogin);
+router.post('/google', authMiddleware, AuthController.googleLogin);
 
-router.get('/me', authMiddleware, getMe);
+router.get('/me', authMiddleware, AuthController.getMe);
 
 module.exports = router;
